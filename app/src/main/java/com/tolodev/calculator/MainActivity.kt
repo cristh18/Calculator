a395d3623c6bdb729fea270b4d170703883d6f0e
Cristhian Tolosa <cristhian.tolosa@rappi.com>
2018-02-14 18:20:56 -0500

    Create another branch

--- app/src/main/java/com/tolodev/calculator/MainActivity.kt
+++ app/src/main/java/com/tolodev/calculator/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.text.Editable
+import android.text.TextUtils
 import android.util.Log
 import android.widget.EditText
-import android.widget.Toast
+import java.util.regex.Pattern
 
-class MainActivity : AppCompatActivity(), CalculatorAdapter.ViewHolder.OptionListener, TextWatcherListener {
+class MainActivity : AppCompatActivity(), CalculatorAdapter.ViewHolder.OptionListener {
 
     private lateinit var editText: EditText
     private lateinit var recyclerView: RecyclerView
@@ -21,17 +21,12 @@ class MainActivity : AppCompatActivity(), CalculatorAdapter.ViewHolder.OptionLis
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
-        initListeners()
         setupView()
     }
 
     private fun initView() {
         editText = findViewById(R.id.editText_field)
         recyclerView = findViewById(R.id.recyclerView_options)
-    }
-
-    private fun initListeners(){
-        editText.addTextChangedListener(this)
     }
 
     private fun setupView() {
@@ -53,50 +48,75 @@ class MainActivity : AppCompatActivity(), CalculatorAdapter.ViewHolder.OptionLis
         return options
     }
 
-    /**
-     *
-     */
-
     override fun optionSelected(value: String) {
-//        if (value != getString(R.string.option_equals)) {
-//            currentOperation = currentOperation.plus(value)
-//            editText.setText(currentOperation)
-//        } else {
-////            val operators = currentOperation.split("([\\/\\+\\-\\*])")
-////            for (s in operators){
-////
-////            }
-//
-//            if (currentOperation.contains(getString(R.string.option_plus))) {
-//                val operators = currentOperation.split(getString(R.string.option_plus))
-//                val result = operators.sumByDouble { (it).toDouble() }
-//                currentOperation = result.toString()
-//                editText.setText(currentOperation)
-//            } else if () {
-//
-//            } else if () {
-//
-//            } else if () {
-//
-//            }
-//        }
+        currentOperation += value
+        editText.setText(currentOperation)
+//        if (getOperators().contains(value) && value == getString(R.string.option_equals)) {
 
-        if (value != getString(R.string.option_equals)) {
-            currentOperation = currentOperation.plus(value)
+        if (getOperators().contains(value)) {
+
+
+            val pattern = Pattern.compile("([\\/\\+\\-\\*])")
+            val matcher = pattern.matcher(currentOperation)
+            if (matcher.find()) {
+                Log.e(this::javaClass.name, "Numbers: ".plus(currentOperation))
+                val operator = matcher.group(1)
+                Log.e(this::javaClass.name, "Operator: ".plus(operator))
+                val numbers = currentOperation.split(operator)
+
+//                if (numbers.size < 3 && !TextUtils.isEmpty(numbers[0]) && !TextUtils.isEmpty(numbers[1])) {
+//                    currentOperation = getResult(operator, numbers).toString()
+//                } else if (numbers.size >= 3) {
+//                    currentOperation += (numbers[2])
+//                }
+
+                if (numbers.size == 2) {
+                    if (!TextUtils.isEmpty(numbers[0]) && !TextUtils.isEmpty(numbers[1])) {
+                        currentOperation = getResult(operator, numbers).toString()
+                    }
+                } else {
+//                    if (!TextUtils.isEmpty(numbers[0]) && !TextUtils.isEmpty(numbers[1]) && TextUtils.isEmpty(numbers[2])) {
+//                        val lastWord = currentOperation.substring(currentOperation.length - 1, currentOperation.length)
+//                        optionSelected(lastWord)
+//                    }
+                }
+
+                editText.setText(currentOperation)
+            }
+        } else if (value == getString(R.string.option_clear)) {
+            currentOperation = ""
             editText.setText(currentOperation)
         }
+
     }
 
-    override fun afterTextChanged(s: Editable?) {
-        super.afterTextChanged(s)
-        Toast.makeText(this, "Element: ".plus(s), Toast.LENGTH_SHORT).show()
-
-        if (s.toString().contains("([\\/\\+\\-\\*])")){
-            val operators = currentOperation.split("([\\/\\+\\-\\*])")
-
-            for (operator in operators){
-                Log.e(this::javaClass.name, "Operator: ".plus(operator))
-            }
+    private fun getResult(operator: String, numbers: List<String>): Double {
+        var res = 0.0
+        when (operator) {
+            getString(R.string.option_plus) -> res = sum(numbers[0].toDouble(), numbers[1].toDouble())
+            getString(R.string.option_minus) -> res = subtract(numbers[0].toDouble(), numbers[1].toDouble())
+            getString(R.string.option_product) -> res = multiply(numbers[0].toDouble(), numbers[1].toDouble())
+            getString(R.string.option_division) -> res = divide(numbers[0].toDouble(), numbers[1].toDouble())
         }
+        return res
     }
+
+    private fun getOperators(): MutableList<String> {
+        val operators = mutableListOf<String>()
+        operators.add(getString(R.string.option_plus))
+        operators.add(getString(R.string.option_minus))
+        operators.add(getString(R.string.option_product))
+        operators.add(getString(R.string.option_division))
+        operators.add(getString(R.string.option_percentage))
+        operators.add(getString(R.string.option_equals))
+        return operators
+    }
+
+    private fun sum(numberA: Double, numberB: Double) = numberA + numberB
+
+    private fun subtract(numberA: Double, numberB: Double) = numberA - numberB
+
+    private fun multiply(numberA: Double, numberB: Double) = numberA * numberB
+
+    private fun divide(numberA: Double, numberB: Double) = numberA / numberB
 }
